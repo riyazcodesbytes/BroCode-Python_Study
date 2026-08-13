9584e03fb7a18c43df0afd5ca574e8719bc2ace1
riyazcodesbytes <riyazshaik4me@gmail.com>
2026-08-13 12:11:57 +0530

    First file edited

--- 1_test.py
+++ 1_test.py
@@ -1,2 +1,7 @@
+# This is a single line comment!
+
+"""
+This is a multi-line comment! or a triple quoted string. It can be used as a multi-line comment.
+"""
 print("Hello, World!");
 print("I am a Python Expert!");
